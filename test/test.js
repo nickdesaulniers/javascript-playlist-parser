19712f2d59481b8ff6583720cec4161534cf2582
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-08-12 15:51:45 -0700

    started on m3u parsing

--- test/test.js
+++ test/test.js
@@ -1,9 +1,39 @@
-var assert = require("assert")
-describe('Array', function(){
-    describe('#indexOf()', function(){
-          it('should return -1 when the value is not present', function(){
-                  assert.equal(-1, [1,2,3].indexOf(5));
-                        assert.equal(-1, [1,2,3].indexOf(0));
-                            })
-            })
-})
+var fs = require("fs");
+var assert = require("assert");
+var should = require("chai").should();
+
+var parsers = require("../index");
+var M3U = parsers.M3U;
+
+console.log(M3U.name);
+
+describe("m3u parsing", function () {
+  it("should have a name of m3u", function () {
+    M3U.name.should.equal("m3u");
+  });
+
+  it("should return an array", function () {
+    var parsed = M3U.parse("");
+    parsed.should.be.an("array");
+    parsed.length.should.be.empty;
+  });
+
+  describe("default parsing", function () {
+    it("should return an array of objects", function () {
+      var playlist = fs.readFileSync("./test/url.m3u", { encoding: "utf8" });
+      var parsed = M3U.parse(playlist);
+      parsed.should.be.an("array");
+      parsed.should.not.be.empty;
+      parsed.length.should.equal(2);
+
+      parsed.forEach(function (song) {
+        song.should.be.an("object");
+        song.should.have.ownProperty("location");
+      });
+
+      parsed[0].location.should.equal("http://stream-sd.radioparadise.com:8058");
+      parsed[1].location.should.equal("http://stream-sd.radioparadise.com:8056");
+    });
+  });
+});
+
