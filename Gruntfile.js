e37cbd0332f92a78b99c20bdb1d4ce42e316e51b
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-08-20 14:59:31 -0700

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    coffee: {
+      compileJoined: {
+        files: {
+          "build/parser.js": ["lib/*.coffee"],
+        },
+      },
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: "nyan",
+        },
+        src: ["test/test.js"],
+      },
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-coffee");
+  grunt.loadNpmTasks("grunt-mocha-test");
+
+  grunt.registerTask("default", ["coffee", "mochaTest"]);
+  grunt.registerTask("test", ["mochaTest"]);
+};
